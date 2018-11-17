39cac5ef0182b1ef788a501cbd7d745fd8c186d7
ccc <mchadchay@hotmail.com>
2018-11-18 00:28:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 const path = require('path');
 const app = express();
-var cloudinary = require('cloudinary');
+// var cloudinary = require('cloudinary');
 
 cloudinary.config({ 
     cloud_name: 'hdzvdkljx', 
